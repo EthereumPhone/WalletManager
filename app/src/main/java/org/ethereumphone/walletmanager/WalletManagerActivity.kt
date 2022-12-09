1c89a6c6a651822857e65384159a8bea7dfcd032
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-12-09 18:00:31 +0100

    added WalletManagerApp to app activity

--- app/src/main/java/org/ethereumphone/walletmanager/WalletManagerActivity.kt
+++ app/src/main/java/org/ethereumphone/walletmanager/WalletManagerActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import org.ethereumphone.walletmanager.theme.WalletManagerTheme
+import org.ethereumphone.walletmanager.ui.WalletManagerApp
 
 
-class MainActivity: ComponentActivity()  {
+class WalletManagerActivity: ComponentActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WalletManagerTheme {
-
+            WalletManagerTheme() {
+                WalletManagerApp()
             }
-
         }
     }
 }
+
