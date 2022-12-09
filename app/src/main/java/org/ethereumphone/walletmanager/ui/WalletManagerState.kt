d13d264be6571bdc2e93ad7cf5aaa11f5a1e1f3c
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-12-09 17:57:42 +0100

    Created state class for top level destinations navigation

---
+++ app/src/main/java/org/ethereumphone/walletmanager/ui/WalletManagerState.kt
@@ -0,0 +1,61 @@
+package org.ethereumphone.walletmanager.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.core.os.trace
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import org.ethereumphone.walletmanager.navigation.*
+
+@Composable
+fun rememberWalletManagerAppState(
+    navController: NavHostController = rememberNavController()
+): WalletManagerState {
+    return remember(navController) {
+        WalletManagerState(navController)
+    }
+}
+
+@Stable
+class WalletManagerState(
+    val navController: NavHostController
+) {
+    val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val currentTopLevelDestination: TopLevelDestination?
+        @Composable get() = when(currentDestination?.route) {
+            homeRoute -> TopLevelDestination.HOME
+            swapRoute -> TopLevelDestination.SWAP
+            assetRoute -> TopLevelDestination.ASSET
+            else -> null
+        }
+
+    val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.values().asList()
+
+    fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
+        trace("Navigation: ${topLevelDestination.name}") {
+            val topLevelNavOptions = navOptions {
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+                launchSingleTop = true
+                restoreState = true
+            }
+            when (topLevelDestination) {
+                TopLevelDestination.HOME -> navController.navigateToHome(topLevelNavOptions)
+                TopLevelDestination.SWAP -> navController.navigateToSwap(topLevelNavOptions)
+                TopLevelDestination.ASSET -> navController.navigateToAsset(topLevelNavOptions)
+            }
+        }
+    }
+    fun onBackClick() {
+        navController.popBackStack()
+    }
+}
