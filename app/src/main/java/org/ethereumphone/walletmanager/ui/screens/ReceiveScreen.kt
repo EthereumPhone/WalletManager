04b3660905bfb6d1fea78f1e402665f573cf6659
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2022-12-26 23:07:39 +0100

    Temp. UI and Navigation fix for ReceiveScreen.kt.

--- app/src/main/java/org/ethereumphone/walletmanager/ui/screens/ReceiveScreen.kt
+++ app/src/main/java/org/ethereumphone/walletmanager/ui/screens/ReceiveScreen.kt
@@ -9,22 +9,28 @@ import android.graphics.BitmapFactory
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import net.glxn.qrgen.android.QRCode
+import org.ethereumphone.walletmanager.models.Network
 import org.ethereumphone.walletmanager.theme.*
 import org.ethereumphone.walletmanager.utils.WalletSDK
 import java.io.ByteArrayInputStream
@@ -34,83 +40,108 @@ import java.io.ByteArrayOutputStream
 /**
  * Function to generate bitmap that is a qr code from a string
  */
-fun generateQRCode(input: String, width: Int, height: Int): Bitmap {
-    val stream: ByteArrayOutputStream = QRCode
-        .from(input)
-        .withSize(width, height)
-        .stream()
-    val bytearay = stream.toByteArray()
-    return BitmapFactory.decodeByteArray(bytearay, 0, bytearay.size)
-}
 
 
 @Composable
 fun ReceiveRoute(
-    modifier: Modifier = Modifier,
-    address: String
+    address: String,
+    selectedNetworkState: State<Network>,
+    onBackClick: () -> Unit
 ) {
     ReceiveScreen(
-        modifier = modifier,
-        address = address
+        address = address,
+        selectedNetwork = selectedNetworkState,
+        onBackClick = onBackClick
     )
 }
 
+private val networkStyles: List<NetworkStyle> = NetworkStyle.values().asList()
+
 @Composable
 fun ReceiveScreen(
-    modifier: Modifier = Modifier,
-    address: String
+    address: String,
+    selectedNetwork: State<Network>,
+    onBackClick: () -> Unit
+
 ) {
     val configuration = LocalConfiguration.current
     val context = LocalContext.current
-    WalletManagerTheme {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(25.dp),
-            modifier = modifier
-                .height(LocalConfiguration.current.screenHeightDp.dp)
-                .padding(40.dp)
-
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(horizontal = 20.dp)
+    ) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
         ) {
-            Text(
-                text = "Receive ether",
-                fontWeight = FontWeight.Bold,
-                fontSize = 45.sp,
-                modifier = Modifier.fillMaxWidth(),
-                // Set color to be white
-                color = md_theme_dark_onPrimary
-            )
-            BitmapImage(
-                bitmap = generateQRCode(
-                    input = "ethereum:$address",
-                    width = configuration.screenWidthDp.dpToPx(),
-                    height = configuration.screenWidthDp.dpToPx()
-                ),
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-            Button(
-                shape = RoundedCornerShape(50.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = md_theme_light_primary),
-                modifier = Modifier.fillMaxWidth(),
-                onClick = {
-                    // Copy address to clipboard
-                    val clipboard: ClipboardManager? =
-                        context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
-                    val clip = ClipData.newPlainText("Address", address)
-                    clipboard?.setPrimaryClip(clip)
-
-                    // Toast copy to clipboard
-                    Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT).show()
-                }
+            IconButton(
+                onClick = { onBackClick() },
+                modifier = Modifier.align(Alignment.CenterStart)
             ) {
-                Text(
-                    text = "Copy address",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp,
-                    modifier = Modifier.fillMaxWidth(),
-                    // Set color to be white
-                    color = md_theme_dark_onPrimary
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "return to home screen",
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(32.dp)
                 )
             }
+            Column(
+                modifier = Modifier.align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Send To",
+                    fontSize = 24.sp,
+                    modifier = Modifier
+                        .padding(top = 20.dp),
+                    fontStyle = FontStyle.Normal
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(8.dp)
+                            .clip(shape = CircleShape)
+                            .background(
+                                networkStyles.first { it.networkName == selectedNetwork.value.chainName }.color
+                            )
+                    )
+                    Text(
+                        text = "  "+selectedNetwork.value.chainName,
+                        fontSize = 12.sp,
+                    )
+                }
+            }
+        }
+
+        BitmapImage(
+            bitmap = generateQRCode(
+                input = "ethereum:$address",
+                width = configuration.screenWidthDp.dpToPx(),
+                height = configuration.screenWidthDp.dpToPx()
+            ),
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            shape = RoundedCornerShape(30.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = md_theme_light_primary),
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                // Copy address to clipboard
+                val clipboard: ClipboardManager? =
+                    context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
+                val clip = ClipData.newPlainText("Address", address)
+                clipboard?.setPrimaryClip(clip)
+
+                // Toast copy to clipboard
+                Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            Text(
+                text = "Copy Address"
+            )
         }
     }
 }
@@ -122,11 +153,23 @@ fun Int.dpToPx(): Int {
     return (this * Resources.getSystem().displayMetrics.density).toInt()
 }
 
+fun generateQRCode(input: String, width: Int, height: Int): Bitmap {
+    val stream: ByteArrayOutputStream = QRCode
+        .from(input)
+        .withSize(width, height)
+        .stream()
+    val bytearay = stream.toByteArray()
+    return BitmapFactory.decodeByteArray(bytearay, 0, bytearay.size)
+}
+
 @Composable
 fun BitmapImage(bitmap: Bitmap) {
     Image(
         bitmap = bitmap.asImageBitmap(),
-        contentDescription = "some useful description",
+        contentDescription = "Wallet's Address QR",
+        modifier = Modifier
+            .padding(top = 20.dp)
+            .clip(RoundedCornerShape(20.dp))
     )
 }
 
@@ -135,5 +178,5 @@ fun BitmapImage(bitmap: Bitmap) {
 @Preview
 @Composable
 fun ReceiveScreenPreview() {
-    ReceiveScreen(address = "0x3a4e6ed8b0f02bfbfaa3c6506af2db939ea5798c")
+    //ReceiveScreen(address = "0x3a4e6ed8b0f02bfbfaa3c6506af2db939ea5798c")
 }
