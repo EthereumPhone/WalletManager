69597b6843af60d0ba54a005f6cc425cecc9e4cc
Markus Haas <haas.markus@protonmail.com>
2023-02-20 13:45:42 +0100

    Add in-screen qr code scanner

--- app/src/main/java/org/ethereumphone/walletmanager/ui/screens/SendScreen.kt
+++ app/src/main/java/org/ethereumphone/walletmanager/ui/screens/SendScreen.kt
@@ -7,32 +7,53 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.impl.utils.ContextUtil.getApplicationContext
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Backspace
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.QrCode
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
 import androidx.core.content.ContextCompat
-import com.journeyapps.barcodescanner.CaptureManager
-import com.journeyapps.barcodescanner.CompoundBarcodeView
+import androidx.core.util.Consumer
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.ResultPoint
+import com.journeyapps.barcodescanner.*
 import org.ethereumphone.walletmanager.models.Network
 import org.ethereumphone.walletmanager.theme.*
 import org.ethereumphone.walletmanager.ui.components.InputField
@@ -94,18 +115,7 @@ fun SendScreen(
         launcher.launch(Manifest.permission.CAMERA)
     }
 
-    if(showDialog) {
-        qrCodeDialog(
-            context = context,
-            setShowDialog = {
-                showDialog = false
-            },
-            onDecoded = {
-                address = it.replace("ethereum:", "")
-                showDialog = false
-            },
-        )
-    }
+
     Box(
         modifier = Modifier.background(black)
     ){
@@ -181,11 +191,11 @@ fun SendScreen(
                         colors = if(validSendAddress) InputFiledColors() else InputFiledColors(backgroundColor = Color.Yellow.copy(0.2f)),
                         trailingIcon = {
                             IconButton(
-                                onClick = { showDialog = true },
+                                onClick = { showDialog = !showDialog },
                                 modifier = Modifier.padding(end=16.dp)
                             ) {
                                 Icon(
-                                    imageVector = Icons.Default.QrCode,
+                                    imageVector = if (showDialog) Icons.Default.Close else Icons.Default.QrCode,
                                     contentDescription = "Address by QR",
                                     tint = if(validSendAddress) md_theme_dark_onSurface else Color.Yellow,
                                     modifier = Modifier.size(24.dp)
@@ -204,8 +214,10 @@ fun SendScreen(
                                         address = ensAddr?.hex.toString()
                                     }
                                 } else {
-                                    if (address.isNotEmpty()) validSendAddress = isValidAddress(address) }
-                                if(address.isEmpty()) validSendAddress = true
+                                    if (address.isNotEmpty()) validSendAddress =
+                                        isValidAddress(address)
+                                }
+                                if (address.isEmpty()) validSendAddress = true
                             }
                     ) {
                         address = it
@@ -219,7 +231,18 @@ fun SendScreen(
                 modifier = Modifier.padding(horizontal = 10.dp)
             )
 
-
+            if(showDialog) {
+                qrCodeDialog(context) {
+                    print("QR CODE: $it")
+                    address = it.replace("ethereum:", "")
+                    showDialog = false
+                }
+            } else {
+                InputFieldWithKey {
+                    amount = it
+                }
+            }
+            /*
             InputField(
                 label = {
                     if(amountError) Text("Amount",
@@ -239,6 +262,8 @@ fun SendScreen(
                 amount = it
                 amountError = amount.contains(Regex("-| "))
             }
+             */
+
             if(amountError) Text(
                 text= "Illegal characters (-, _)",
                 color = Color.Red,
@@ -253,7 +278,7 @@ fun SendScreen(
                     .fillMaxWidth()
                     .padding(bottom = 10.dp)
                     .height(56.dp),
-                enabled = amount.isNotEmpty() && address.isNotEmpty() && !amountError,
+                enabled = amount.isNotEmpty() && address.isNotEmpty(),
                 colors = ButtonDefaults.buttonColors(
                     disabledBackgroundColor = Color.Gray,
                     backgroundColor = dark_primary
@@ -284,7 +309,6 @@ fun SendScreen(
                         }
                     }
                 }
-                //onClick = {}
             ) {
                 Text(
                     text = "Send",
@@ -298,9 +322,141 @@ fun SendScreen(
 }
 
 @Composable
+fun InputFieldWithKey(
+    newValue: (String) -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var text by remember { mutableStateOf("") }
+        Text(text = "$text ETH", modifier = Modifier.padding(10.dp))
+        Divider(color = Color.Gray, thickness = 1.dp, modifier = Modifier.fillMaxWidth(0.8f))
+        Spacer(modifier = Modifier.height(10.dp))
+        ethOSKeyboard {
+            text = it
+            newValue(it)
+        }
+
+    }
+}
+
+
+@Composable
+fun ethOSKeyboard(
+    newText: (String) -> Unit
+) {
+    var text by remember { mutableStateOf("") }
+    NumberKeyboard(
+        onNumberClicked = { number ->
+            text += number.toString()
+            newText(text)
+        },
+        onCommaClicked = {
+            if (!text.contains(".")) {
+                text += "."
+                newText(text)
+            }
+        },
+        onBackspaceClicked = {
+            if (text.isNotEmpty()) {
+                text = text.substring(0, text.length - 1)
+                newText(text)
+            }
+        }
+    )
+
+}
+
+@Composable
+fun NumberKeyboard(
+    onNumberClicked: (Int) -> Unit,
+    onCommaClicked: () -> Unit,
+    onBackspaceClicked: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        // Create the first row with 1-3 keys
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            NumberKey(number = 1, onClick = onNumberClicked)
+            NumberKey(number = 2, onClick = onNumberClicked)
+            NumberKey(number = 3, onClick = onNumberClicked)
+        }
+        // Create the second row with 4-6 keys
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            NumberKey(number = 4, onClick = onNumberClicked)
+            NumberKey(number = 5, onClick = onNumberClicked)
+            NumberKey(number = 6, onClick = onNumberClicked)
+        }
+        // Create the third row with 7-9 keys and the comma, 0, and backspace keys
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            NumberKey(number = 7, onClick = onNumberClicked)
+            NumberKey(number = 8, onClick = onNumberClicked)
+            NumberKey(number = 9, onClick = onNumberClicked)
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            CommaKey(onClick = onCommaClicked)
+            NumberKey(number = 0, onClick = onNumberClicked)
+            BackspaceKey(onClick = onBackspaceClicked)
+        }
+    }
+}
+
+@Composable
+fun NumberKey(number: Int, onClick: (Int) -> Unit) {
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .size(64.dp)
+            .background(Color(0xFF24303D))
+            .clickable { onClick(number) },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(number.toString(), fontSize = 24.sp, color = Color.White, fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+fun CommaKey(onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .size(64.dp)
+            .background(Color(0xFF24303D))
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(".", fontSize = 24.sp, color = Color.White, fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+fun BackspaceKey(onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .size(64.dp)
+            .background(Color(0xFF24303D))
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            Icons.Filled.Backspace,
+            contentDescription = "Backspace",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
+
+
+
+@Composable
 fun qrCodeDialog(
     context: Context,
-    setShowDialog: () -> Unit,
     onDecoded: (String) -> Unit
 ) {
     var scanFlag by remember { mutableStateOf(false) }
@@ -328,13 +484,31 @@ fun qrCodeDialog(
             }
         }
     }
-    Dialog(onDismissRequest = { setShowDialog() }) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
         AndroidView(
-            modifier = Modifier,
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .fillMaxHeight(0.8f),
             factory = { compoundBarcodeView },
         )
     }
+
 }
+
+
+@Composable
+@Preview
+fun PreviewScreenKeyboard() {
+    WalletManagerTheme {
+        InputFieldWithKey()
+    }
+}
+
 /*
 @ExperimentalComposeUiApi
 @Composable
@@ -353,6 +527,9 @@ fun PreviewMintingScreen() {
             override val value: Network
                 get() = mainnetNetwork
         }
-        SendScreen(selectedNetworkState = selectedNetwork)
+        SendScreen(selectedNetworkState = selectedNetwork) {
+
+        }
     }
-}*/
+}
+*/
