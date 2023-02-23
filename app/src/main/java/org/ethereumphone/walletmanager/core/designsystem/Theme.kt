cb768602d9cddd2f2045b7e8230b3873d337406c
nceornea <nicola.ceornea@hotmail.com>
2023-02-23 22:16:02 +0100

    changed Statusbar color to match background

--- app/src/main/java/org/ethereumphone/walletmanager/core/designsystem/Theme.kt
+++ app/src/main/java/org/ethereumphone/walletmanager/core/designsystem/Theme.kt
@@ -4,6 +4,8 @@ package org.ethereumphone.walletmanager.core.designsystem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import com.google.accompanist.systemuicontroller.SystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
 val DarkDefaultColorScheme = darkColorScheme(
@@ -15,6 +17,11 @@ val DarkDefaultColorScheme = darkColorScheme(
 fun WmTheme(
     content: @Composable () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = background
+    )
+
     MaterialTheme(
         content = content,
         typography = WmTypography
