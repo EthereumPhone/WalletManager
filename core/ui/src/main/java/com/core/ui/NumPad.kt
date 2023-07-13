edca0f16e3a8d59344d55245f944ebf417e03cc1
emunsi <emunsi123@outlook.com>
2023-07-13 14:10:56 +0100

    added components

---
+++ core/ui/src/main/java/com/core/ui/NumPad.kt
@@ -0,0 +1,133 @@
+package com.core.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Backspace
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.FiberManualRecord
+import androidx.compose.material.icons.rounded.Backpack
+import androidx.compose.material.icons.rounded.Backspace
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NumPad(
+        value: String,
+        contentColor: Color = NumPadDefaults.contentColor,
+        modifier: Modifier = Modifier,
+        onValueChange: (String) -> Unit
+) {
+    val numPadButtons =  NumPadButtons.values().asList()
+
+    LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            modifier = modifier,
+            contentPadding = PaddingValues(vertical = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+    ) {
+        itemsIndexed(numPadButtons) { index, button ->
+            val fixedIndex = (index+1).mod(11)
+            Box(
+                    modifier = Modifier
+                            .clickable {
+                                when(button) {
+                                    NumPadButtons.COMMA -> {
+                                        if (!value.contains(".")) {
+                                            if (value == "") {
+                                                onValueChange("0.")
+                                            } else {
+                                                onValueChange("$value.")
+                                            }
+                                        }
+                                    }
+                                    NumPadButtons.UNDO -> onValueChange(value.dropLast(1))
+                                    else -> onValueChange(value + fixedIndex.toString())
+                                }
+                            },
+                    contentAlignment = Alignment.Center
+            ) {
+                var input = when(button) {
+                    NumPadButtons.UNDO -> Icons.Rounded.Backspace
+                    NumPadButtons.COMMA -> "·"
+                    else -> fixedIndex.toString()
+                }
+
+                if(input is ImageVector) {
+                    Box(
+                            modifier = Modifier.size(32.dp)
+                    ) {
+                        IconAsText(
+                                icon = input,
+                                modifier = Modifier.align(Alignment.BottomCenter)
+                        )
+                    }
+                }
+                if(input is String) {
+                    Text(
+                            text = input,
+                            fontSize = NumPadDefaults.fontSize,
+                            fontWeight = FontWeight.Bold,
+                            color = contentColor,
+                    )
+                }
+            }
+        }
+    }
+}
+
+object NumPadDefaults {
+    val contentColor = Color(0xFF24303D)
+    val fontSize = 24.sp
+}
+
+enum class NumPadButtons {
+    DIGIT_1,
+    DIGIT_2,
+    DIGIT_3,
+    DIGIT_4,
+    DIGIT_5,
+    DIGIT_6,
+    DIGIT_7,
+    DIGIT_8,
+    DIGIT_9,
+    COMMA,
+    DIGIT_0,
+    UNDO
+}
+
+@Preview
+@Composable
+fun PreviewNumPad() {
+    NumPad(
+            value = "",
+            modifier = Modifier
+    ) {}
+}
--- feature/send/src/main/java/com/feature/send/SendScreen.kt
+++ feature/send/src/main/java/com/feature/send/SendScreen.kt
@@ -1,6 +1,58 @@
 package com.feature.send
 
+import android.graphics.Bitmap
 import androidx.compose.runtime.Composable
+import android.view.Gravity
+import android.view.Surface
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.ArrowRight
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.QrCode
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.DialogWindowProvider
+import org.ethereumphone.walletmanager.core.designsystem.WmTheme
+import org.ethereumphone.walletmanager.core.designsystem.onPrimary
+import org.ethereumphone.walletmanager.core.ui.SwipeIndicator
+import org.ethereumphone.walletmanager.core.ui.WmSwipeButton
+import org.ethereumphone.walletmanager.core.ui.WmTextField
+import org.ethereumphone.walletmanager.feature_home.model.WalletAmount
+import java.util.Hashtable
 
 @Composable
 fun SendRoute() {
@@ -8,6 +60,15 @@ fun SendRoute() {
 }
 
 @Composable
-fun SendScreen() {
+fun SendScreen(
 
+) {
+    Column() {
+
+        //Values
+        //Numpad
+        //Button
+
+    }
 }
+
