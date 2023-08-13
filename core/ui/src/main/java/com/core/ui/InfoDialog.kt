2b68bd889c6e7a5e8eb0250177d509a803e24c17
emunsi <emunsi123@outlook.com>
2023-08-13 22:23:33 +0200

    Receive Screen w/ InfoDialog implementation

---
+++ core/ui/src/main/java/com/core/ui/InfoDialog.kt
@@ -0,0 +1,101 @@
+package com.core.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
+@Composable
+fun InfoDialog(
+    title: String,
+    text: String,
+    setShowDialog: () -> Unit,
+){
+    Dialog(onDismissRequest = { setShowDialog() }) {
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            color = Color(0xFF24303D),
+            contentColor = Color.White,
+            tonalElevation = 4.dp
+        ) {
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    modifier = Modifier.padding(28.dp),
+
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.Close,
+                            contentDescription = "",
+                            tint = Color(0xFF24303D),
+                            modifier = Modifier
+                                .width(30.dp)
+                                .height(30.dp)
+
+                        )
+                        Text(
+                            text = title,
+                            style = TextStyle(
+                                fontSize = 24.sp,
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                        Icon(
+                            imageVector = Icons.Rounded.Close,
+                            contentDescription = "Close",
+                            tint = Color(0xFF24303D),//Color.White,
+                            modifier = Modifier
+                                .width(16.dp)
+                                .height(16.dp)
+                                .clickable { setShowDialog() }
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    Text(
+                        text = text,
+                        fontSize = 16.sp,
+                        color = Color(0xFF9FA2A5),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Normal,
+                    )
+
+                }
+            }
+        }
+    }
+}
