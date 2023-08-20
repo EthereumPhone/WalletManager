0540931ff4ba5659ff47836ce965dc8e69a08fff
Markus Haas <haas.markus@protonmail.com>
2023-08-20 14:40:02 +0200

    Update for webview to be main thread

--- uniswap-routing-sdk/src/main/java/org/ethosmobile/uniswap_routing_sdk/UniswapRoutingSDK.kt
+++ uniswap-routing-sdk/src/main/java/org/ethosmobile/uniswap_routing_sdk/UniswapRoutingSDK.kt
@@ -2,6 +2,8 @@ package org.ethosmobile.uniswap_routing_sdk
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.webkit.ConsoleMessage
 import android.webkit.WebChromeClient
@@ -28,61 +30,62 @@ class UniswapRoutingSDK(
 
     @SuppressLint("SetJavaScriptEnabled")
     fun getQuote(inputToken: Token, outputToken: Token, amountIn: Double, receiverAddress: String): CompletableFuture<Double> {
-        val wv = WebView(context)
-        wv.settings.javaScriptEnabled = true
-        wv.settings.allowFileAccess = true
-        wv.settings.domStorageEnabled = true // Turn on DOM storage
-        wv.settings.databaseEnabled = true
-
         val completableFuture: CompletableFuture<Double> = CompletableFuture()
+        Handler(Looper.getMainLooper()).post {
+            val wv = WebView(context)
+            wv.settings.javaScriptEnabled = true
+            wv.settings.allowFileAccess = true
+            wv.settings.domStorageEnabled = true // Turn on DOM storage
+            wv.settings.databaseEnabled = true
 
 
-        val bundleJs = getAssetContent(context.assets.open("getquote.js"))
 
-        wv.webChromeClient = object : WebChromeClient() {
-            override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
-                println(consoleMessage.message())
-                if (consoleMessage.message().contains("OUT_CALLDATA")) {
-                    val callData = consoleMessage.message().split("OUT_CALLDATA:")[1]
-                    completableFuture.complete(callData.toDouble())
+            val bundleJs = getAssetContent(context.assets.open("getquote.js"))
+
+            wv.webChromeClient = object : WebChromeClient() {
+                override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+                    println(consoleMessage.message())
+                    if (consoleMessage.message().contains("OUT_CALLDATA")) {
+                        val callData = consoleMessage.message().split("OUT_CALLDATA:")[1]
+                        completableFuture.complete(callData.toDouble())
+                    }
+                    return true
                 }
-                return true
             }
+
+            val output = StringBuilder()
+            output.append("<script type='text/javascript'>\n")
+            output.append(bundleJs)
+            output.append("</script>\n")
+            var realJs = output.toString()
+                .replace("CHAINID_INPUT_TOKEN", inputToken.chainId.toString())
+                .replace("TOKEN_INPUT_ADDRESS", inputToken.address)
+                .replace("DECIMALS_INPUT_TOKEN", inputToken.decimals.toString())
+                .replace("SYMBOL_INPUT_TOKEN", inputToken.symbol)
+                .replace("NAME_INPUT_TOKEN", inputToken.name)
+                .replace("CHAINID_OUTPUT_TOKEN", outputToken.chainId.toString())
+                .replace("TOKEN_OUTPUT_ADDRESS", outputToken.address)
+                .replace("DECIMALS_OUTPUT_TOKEN", outputToken.decimals.toString())
+                .replace("SYMBOL_OUTPUT_TOKEN", outputToken.symbol)
+                .replace("NAME_OUTPUT_TOKEN", outputToken.name)
+                .replace("CHOSEN_IN_AMOUNT", amountIn.toString())
+                .replace("RECEIVER_ADDRESS", receiverAddress)
+                .replace("RPC_PROVIDER_URL", "https://eth-mainnet.g.alchemy.com/v2/lZSeyaiKTV9fKK3kcYYt9CxDZDobSv_Z")
+
+            realJs = if (inputToken == ETH_MAINNET) {
+                realJs.replace("CHOSEN_IN_TOKEN", "ETH")
+            } else {
+                realJs.replace("CHOSEN_IN_TOKEN", "USDC_TOKEN")
+            }
+
+            realJs = if (outputToken == ETH_MAINNET) {
+                realJs.replace("CHOSEN_OUT_TOKEN", "ETH")
+            } else {
+                realJs.replace("CHOSEN_OUT_TOKEN", "DAI_TOKEN")
+            }
+
+            wv.loadDataWithBaseURL("file:///android_asset/index.html", realJs, "text/html", "utf-8", null)
         }
-
-        val output = StringBuilder()
-        output.append("<script type='text/javascript'>\n")
-        output.append(bundleJs)
-        output.append("</script>\n")
-        var realJs = output.toString()
-            .replace("CHAINID_INPUT_TOKEN", inputToken.chainId.toString())
-            .replace("TOKEN_INPUT_ADDRESS", inputToken.address)
-            .replace("DECIMALS_INPUT_TOKEN", inputToken.decimals.toString())
-            .replace("SYMBOL_INPUT_TOKEN", inputToken.symbol)
-            .replace("NAME_INPUT_TOKEN", inputToken.name)
-            .replace("CHAINID_OUTPUT_TOKEN", outputToken.chainId.toString())
-            .replace("TOKEN_OUTPUT_ADDRESS", outputToken.address)
-            .replace("DECIMALS_OUTPUT_TOKEN", outputToken.decimals.toString())
-            .replace("SYMBOL_OUTPUT_TOKEN", outputToken.symbol)
-            .replace("NAME_OUTPUT_TOKEN", outputToken.name)
-            .replace("CHOSEN_IN_AMOUNT", amountIn.toString())
-            .replace("RECEIVER_ADDRESS", receiverAddress)
-            .replace("RPC_PROVIDER_URL", "https://eth-mainnet.g.alchemy.com/v2/lZSeyaiKTV9fKK3kcYYt9CxDZDobSv_Z")
-
-        realJs = if (inputToken == ETH_MAINNET) {
-            realJs.replace("CHOSEN_IN_TOKEN", "ETH")
-        } else {
-            realJs.replace("CHOSEN_IN_TOKEN", "USDC_TOKEN")
-        }
-
-        realJs = if (outputToken == ETH_MAINNET) {
-            realJs.replace("CHOSEN_OUT_TOKEN", "ETH")
-        } else {
-            realJs.replace("CHOSEN_OUT_TOKEN", "DAI_TOKEN")
-        }
-
-        wv.loadDataWithBaseURL("file:///android_asset/index.html", realJs, "text/html", "utf-8", null)
-
 
         return completableFuture
     }
@@ -156,73 +159,76 @@ class UniswapRoutingSDK(
 
     @SuppressLint("SetJavaScriptEnabled")
     fun getAllRouter(inputToken: Token, outputToken: Token, amountIn: Double, receiverAddress: String): CompletableFuture<List<String>> {
-        val wv = WebView(context)
-        wv.settings.javaScriptEnabled = true
-        wv.settings.allowFileAccess = true
-        wv.settings.domStorageEnabled = true // Turn on DOM storage
-        wv.settings.databaseEnabled = true
-
         val completableFuture: CompletableFuture<List<String>> = CompletableFuture()
+        Handler(Looper.getMainLooper()).post {
+            val wv = WebView(context)
+            wv.settings.javaScriptEnabled = true
+            wv.settings.allowFileAccess = true
+            wv.settings.domStorageEnabled = true // Turn on DOM storage
+            wv.settings.databaseEnabled = true
 
 
-        val bundleJs = getAssetContent(context.assets.open("bundledRouting.js"))
 
-        wv.webChromeClient = object : WebChromeClient() {
-            override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
-                Log.d("UniswapRoutingSDK", consoleMessage.message())
-                if (consoleMessage.message().contains("OUT_CALLDATA")) {
-                    val callData = consoleMessage.message().split("OUT_CALLDATA:")[1]
-                    val jsonArray = JSONArray(callData)
-                    val list = ArrayList<String>()
-                    for (i in 0 until jsonArray.length()) {
-                        list.add(jsonArray.getJSONObject(i).getString("address"))
+            val bundleJs = getAssetContent(context.assets.open("bundledRouting.js"))
+
+            wv.webChromeClient = object : WebChromeClient() {
+                override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+                    Log.d("UniswapRoutingSDK", consoleMessage.message())
+                    if (consoleMessage.message().contains("OUT_CALLDATA")) {
+                        val callData = consoleMessage.message().split("OUT_CALLDATA:")[1]
+                        val jsonArray = JSONArray(callData)
+                        val list = ArrayList<String>()
+                        for (i in 0 until jsonArray.length()) {
+                            list.add(jsonArray.getJSONObject(i).getString("address"))
+                        }
+                        completableFuture.complete(list)
                     }
-                    completableFuture.complete(list)
+                    return true
                 }
-                return true
             }
-        }
 
-        val output = StringBuilder()
-        output.append("<script type='text/javascript'>\n")
-        output.append(bundleJs)
-        output.append("</script>\n")
-        var realJs = output.toString()
-            .replace("CHAINID_INPUT_TOKEN", inputToken.chainId.toString())
-            .replace("TOKEN_INPUT_ADDRESS", inputToken.address)
-            .replace("DECIMALS_INPUT_TOKEN", inputToken.decimals.toString())
-            .replace("SYMBOL_INPUT_TOKEN", inputToken.symbol)
-            .replace("NAME_INPUT_TOKEN", inputToken.name)
-            .replace("CHAINID_OUTPUT_TOKEN", outputToken.chainId.toString())
-            .replace("TOKEN_OUTPUT_ADDRESS", outputToken.address)
-            .replace("DECIMALS_OUTPUT_TOKEN", outputToken.decimals.toString())
-            .replace("SYMBOL_OUTPUT_TOKEN", outputToken.symbol)
-            .replace("NAME_OUTPUT_TOKEN", outputToken.name)
-            .replace("CHOSEN_IN_AMOUNT", amountIn.toString())
-            .replace("RECEIVER_ADDRESS", receiverAddress)
-            .replace("RPC_PROVIDER_URL", web3RPC)
+            val output = StringBuilder()
+            output.append("<script type='text/javascript'>\n")
+            output.append(bundleJs)
+            output.append("</script>\n")
+            var realJs = output.toString()
+                .replace("CHAINID_INPUT_TOKEN", inputToken.chainId.toString())
+                .replace("TOKEN_INPUT_ADDRESS", inputToken.address)
+                .replace("DECIMALS_INPUT_TOKEN", inputToken.decimals.toString())
+                .replace("SYMBOL_INPUT_TOKEN", inputToken.symbol)
+                .replace("NAME_INPUT_TOKEN", inputToken.name)
+                .replace("CHAINID_OUTPUT_TOKEN", outputToken.chainId.toString())
+                .replace("TOKEN_OUTPUT_ADDRESS", outputToken.address)
+                .replace("DECIMALS_OUTPUT_TOKEN", outputToken.decimals.toString())
+                .replace("SYMBOL_OUTPUT_TOKEN", outputToken.symbol)
+                .replace("NAME_OUTPUT_TOKEN", outputToken.name)
+                .replace("CHOSEN_IN_AMOUNT", amountIn.toString())
+                .replace("RECEIVER_ADDRESS", receiverAddress)
+                .replace("RPC_PROVIDER_URL", web3RPC)
 
-        realJs = if (inputToken == ETH_MAINNET) {
-            realJs.replace("CHOSEN_IN_TOKEN", "ETH")
-        } else {
-            realJs.replace("CHOSEN_IN_TOKEN", "USDC_TOKEN")
-        }
-
-        realJs = if (outputToken == ETH_MAINNET) {
-            realJs.replace("CHOSEN_OUT_TOKEN", "ETH")
-        } else {
-            realJs.replace("CHOSEN_OUT_TOKEN", "DAI_TOKEN")
-        }
-
-        wv.loadDataWithBaseURL("file:///android_asset/index.html", realJs, "text/html", "utf-8", null)
-
-        // Make completableFuture complete exceptionally if it hasn't completed after 30 seconds
-        Thread {
-            Thread.sleep(30000)
-            if (!completableFuture.isDone) {
-                completableFuture.completeExceptionally(Exception("Timeout"))
+            realJs = if (inputToken == ETH_MAINNET) {
+                realJs.replace("CHOSEN_IN_TOKEN", "ETH")
+            } else {
+                realJs.replace("CHOSEN_IN_TOKEN", "USDC_TOKEN")
             }
-        }.start()
+
+            realJs = if (outputToken == ETH_MAINNET) {
+                realJs.replace("CHOSEN_OUT_TOKEN", "ETH")
+            } else {
+                realJs.replace("CHOSEN_OUT_TOKEN", "DAI_TOKEN")
+            }
+
+            wv.loadDataWithBaseURL("file:///android_asset/index.html", realJs, "text/html", "utf-8", null)
+
+            // Make completableFuture complete exceptionally if it hasn't completed after 30 seconds
+            Thread {
+                Thread.sleep(30000)
+                if (!completableFuture.isDone) {
+                    completableFuture.completeExceptionally(Exception("Timeout"))
+                }
+            }.start()
+        }
+
 
         return completableFuture
     }
