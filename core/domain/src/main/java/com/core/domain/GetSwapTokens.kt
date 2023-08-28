0f17648bbac077d42c449690eb55a3d1f9336a42
Markus Haas <haas.markus@protonmail.com>
2023-08-28 22:38:36 +0300

    Fix tokenBalances for swap

--- core/domain/src/main/java/com/core/domain/GetSwapTokens.kt
+++ core/domain/src/main/java/com/core/domain/GetSwapTokens.kt
@@ -6,7 +6,9 @@ import com.core.data.repository.TokenMetadataRepository
 import com.core.model.TokenAsset
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import java.math.BigDecimal
 import javax.inject.Inject
+import kotlin.math.pow
 
 class GetSwapTokens @Inject constructor(
     private val tokenMetadataRepository: TokenMetadataRepository,
@@ -32,12 +34,14 @@ class GetSwapTokens @Inject constructor(
 
             val erc20Assets = metadata.map { tokenMetadata ->
                 val tokenBalance = erc20Amount.find { tokenMetadata.contractAddress.lowercase() == it.contractAddress.lowercase() }
+                val scale = 10.0.pow(tokenMetadata.decimals).toBigDecimal()
+                val truncatedValue = tokenBalance?.tokenBalance?.setScale(tokenMetadata.decimals, BigDecimal.ROUND_DOWN)
                 TokenAsset(
                     address = tokenMetadata.contractAddress,
                     chainId = tokenMetadata.chainId,
                     symbol = tokenMetadata.symbol,
                     name = tokenMetadata.name,
-                    balance = tokenBalance?.tokenBalance?.toDouble() ?: 0.0
+                    balance = (truncatedValue?.div(scale))?.toDouble() ?: 0.0
                 )
             }
             if(query.isEmpty()) {
