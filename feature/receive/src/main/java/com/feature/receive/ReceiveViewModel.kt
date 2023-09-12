de6dd4c6af8c9e98f4a873ee57055d7e6ba8b9f9
Nicola-Ceornea <43230854+Nicola-Ceornea@users.noreply.github.com>
2023-09-12 15:47:51 +0200

    Fixed QRCode Generation

--- feature/receive/src/main/java/com/feature/receive/ReceiveViewModel.kt
+++ feature/receive/src/main/java/com/feature/receive/ReceiveViewModel.kt
@@ -1,13 +1,18 @@
 package com.feature.receive
 
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.core.data.repository.UserDataRepository
+import com.core.model.UserData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import net.glxn.qrgen.android.QRCode
+import net.glxn.qrgen.core.image.ImageType
+import java.io.ByteArrayOutputStream
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,12 +21,12 @@ class ReceiveViewModel @Inject constructor(
 
     ): ViewModel() {
 
-    val userAddress: StateFlow<String> =
-        userDataRepository.userData.map {
-            it.walletAddress
-        }.stateIn(
+    val userData = userDataRepository.userData
+        .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = ""
+            initialValue = UserData("")
         )
+
 }
+
