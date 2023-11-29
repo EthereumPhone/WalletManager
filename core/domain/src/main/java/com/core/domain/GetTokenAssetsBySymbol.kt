e9874aa758aa19b50216ed61e8a7bedcdac6c24a
Nicola-Ceornea <43230854+Nicola-Ceornea@users.noreply.github.com>
2023-11-29 09:38:00 +0100

    fixes

--- core/domain/src/main/java/com/core/domain/GetTokenAssetsBySymbol.kt
+++ core/domain/src/main/java/com/core/domain/GetTokenAssetsBySymbol.kt
@@ -2,20 +2,17 @@ package com.core.domain
 
 import com.core.data.repository.TokenBalanceRepository
 import com.core.data.repository.TokenMetadataRepository
-import com.core.model.NetworkChain
 import com.core.model.TokenAsset
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.filter
 import javax.inject.Inject
 import kotlin.math.pow
 
-class GetTokenAssetsByNetwork @Inject constructor(
+class GetTokenAssetsBySymbol @Inject constructor(
     private val tokenMetadataRepository: TokenMetadataRepository,
     private val tokenBalanceRepository: TokenBalanceRepository
 ) {
-
-    operator fun invoke(chainId: Int = 0): Flow<List<TokenAsset>> =
+    operator fun invoke(symbol: String? = ""): Flow<List<TokenAsset>> =
         combine(
             tokenBalanceRepository.getTokensBalances(),
             tokenMetadataRepository.getTokensMetadata()
@@ -29,20 +26,20 @@ class GetTokenAssetsByNetwork @Inject constructor(
             }
             val tokenAssets = pairedList
                 .map { (tokenBalance, tokenMetadata) ->
-                TokenAsset(
-                    address = tokenBalance.contractAddress,
-                    chainId = tokenBalance.chainId,
-                    symbol = tokenMetadata.symbol,
-                    name = tokenMetadata.name,
-                    balance = tokenBalance.tokenBalance.divide(
-                        (10.0.pow(tokenMetadata.decimals)).toBigDecimal()).toDouble(),
-                    decimals = tokenMetadata.decimals
-                )
-            }
+                    TokenAsset(
+                        address = tokenBalance.contractAddress,
+                        chainId = tokenBalance.chainId,
+                        symbol = tokenMetadata.symbol,
+                        name = tokenMetadata.name,
+                        balance = tokenBalance.tokenBalance.divide(
+                            (10.0.pow(tokenMetadata.decimals)).toBigDecimal()).toDouble(),
+                        decimals = tokenMetadata.decimals
+                    )
+                }
 
-            if(chainId != 0) {
+            if(symbol != "") {
                 tokenAssets.filter {
-                    it.chainId == chainId
+                    it.symbol == symbol
                 }
             } else {
                 tokenAssets
