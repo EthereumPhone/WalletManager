8d06adc8eee5f0d3194db228a0f3bd96156b009e
Markus Haas <haas.markus@protonmail.com>
2024-01-02 15:02:51 +0100

    Add spam tokens

--- core/data/src/main/java/com/core/data/util/SpamTokens.kt
+++ core/data/src/main/java/com/core/data/util/SpamTokens.kt
@@ -4,5 +4,7 @@ package com.core.data.util
 val spamTokens = listOf(
     "0x7b5b1b76747aa6cf4bb443ca7ee6324867fe3ed2",
     "0xe751e33e17cb827eaadf567e6826a01fe274c2e9",
-    "0x8955f4a520015B701803aA3bcf070DbC8fb9134d"
+    "0x8955f4a520015b701803aa3bcf070dbc8fb9134d",
+    "0x09e18590e8f76b6cf471b3cd75fe1a1a9d2b2c2b",
+    "0x1d987200df3b744cfa9c14f713f5334cb4bc4d5d"
 )
