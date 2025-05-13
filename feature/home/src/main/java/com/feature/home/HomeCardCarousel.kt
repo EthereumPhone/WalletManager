656aeae7327c9dd31a3dfc3cc8f6ad09158f3504
emunsi <munsielie@gmail.com>
2025-05-13 19:06:38 +0200

    added moonpay code

--- feature/home/src/main/java/com/feature/home/HomeCardCarousel.kt
+++ feature/home/src/main/java/com/feature/home/HomeCardCarousel.kt
@@ -345,7 +345,6 @@ fun HomeScreen2(
                 IconButton(modifier = Modifier.clip(RoundedCornerShape(0.dp)).width(100.dp).height(50.dp).padding(bottom = 8.dp),
                     onClick = {
                         if (isOffline){
-
                             context.showCustomToast(
                             message = "No internet connection!",
                             fontFamily = PitagonsSans,
@@ -414,7 +413,7 @@ fun HomeScreen2(
                 Spacer(modifier = Modifier.width(8.dp))
                 IconButton(modifier = Modifier.clip(RoundedCornerShape(0.dp)).width(100.dp).height(50.dp).padding(bottom = 8.dp),
                     onClick = {
-                        /*if (userData is WalletDataUiState.Success) {
+                        if (userData is WalletDataUiState.Success) {
                             val address = userData.userData.walletAddress
                             scope.launch {
                                 val json = Uri.encode("{\"eth\":\"$address\"}")
@@ -423,7 +422,7 @@ fun HomeScreen2(
                                     uriHandler.openUri(uri)
                                 }
                             }
-                        }*/
+                        }
                     }
                 ) {
                     Column(
