74b315576f735574e2003a19a967ffb261b15947
emunsi <munsielie@gmail.com>
2025-05-15 18:48:02 +0200

    added fade animations to SendScreen

--- feature/send/src/main/java/com/feature/send/navigation/sendNavigation.kt
+++ feature/send/src/main/java/com/feature/send/navigation/sendNavigation.kt
@@ -5,6 +5,10 @@ import android.util.Log
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -55,7 +59,33 @@ fun NavGraphBuilder.sendScreen(
                 type = NavType.StringType
                 defaultValue = ""
             }
+        ),
+        enterTransition = { fadeIn(
+            animationSpec = tween(
+                durationMillis = 400,
+                easing = FastOutSlowInEasing
+            )
         )
+        },
+        exitTransition = { fadeOut(
+            animationSpec = tween(
+                durationMillis = 400,
+                easing = FastOutSlowInEasing
+            )
+        )
+        },
+        popEnterTransition = {
+            fadeIn(animationSpec = tween(
+                durationMillis = 400,
+                easing = FastOutSlowInEasing
+            ))
+        },
+        popExitTransition = {
+            fadeOut(animationSpec = tween(
+                durationMillis = 400,
+                easing = FastOutSlowInEasing
+            ))
+        }
     ) { backStackEntry ->
         val address = backStackEntry.arguments?.getString("address")
         val tokenId = backStackEntry.arguments?.getString("tokenId")
