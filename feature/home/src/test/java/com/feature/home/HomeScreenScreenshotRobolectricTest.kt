1ef7020813df907d0029e0738e7d98f975772edf
emunsi <emunsi123@outlook.com>
2025-11-26 17:44:09 +0100

    update Screenshot test

--- feature/home/src/test/java/com/feature/home/HomeScreenScreenshotRobolectricTest.kt
+++ feature/home/src/test/java/com/feature/home/HomeScreenScreenshotRobolectricTest.kt
@@ -1,6 +1,17 @@
 package com.feature.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onRoot
 import com.github.takahirom.roborazzi.captureRoboImage
 import org.junit.Rule
@@ -11,7 +22,10 @@ import org.robolectric.annotation.Config
 import org.robolectric.annotation.GraphicsMode
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [34])
+@Config(
+    sdk = [34],
+    qualifiers = "w480dp-h480dp-port-hdpi"
+)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
 class HomeScreenScreenshotRobolectricTest {
 
@@ -20,21 +34,34 @@ class HomeScreenScreenshotRobolectricTest {
 
     @Test
     fun snapshot_home_default_robo() {
+        val widthPx = 720
+        val heightPx = 720
+        val dpi = 240
+        val density = dpi / 160f
+
         composeRule.setContent {
-            HomeScreen2(
-                groupedAssetsUiState = GroupedAssetsUiState.Loading,
-                navigateToSwap = {},
-                navigateToSend = {},
-                navigateToLog = {},
-                navigateToReceive = {},
-                navigateToPayMaster = {},
-                isOffline = false,
-                hasTransfer = false
-            )
+            CompositionLocalProvider(LocalDensity provides Density(density = density, fontScale = 1f)) {
+                Box(
+                    Modifier
+                        .size((widthPx / density).dp, (heightPx / density).dp)
+                        .semantics { testTag = "screenshot" }
+                ) {
+                    HomeScreen2(
+                        groupedAssetsUiState = GroupedAssetsUiState.Loading,
+                        navigateToSwap = {},
+                        navigateToSend = {},
+                        navigateToLog = {},
+                        navigateToReceive = {},
+                        navigateToPayMaster = {},
+                        isOffline = false,
+                        hasTransfer = false
+                    )
+                }
+            }
         }
 
         composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage(filePath = "screenshot/snapshot_home_default_robo.png")
+        composeRule.onNodeWithTag("screenshot").captureRoboImage(filePath = "screenshot/snapshot_home_default_robo.png")
     }
 }
 
